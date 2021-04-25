e4b7c5133fc2f2522a92ceff8bef86af2535436e
Siddharth Diwan <sidwan02@gmail.com>
2021-04-25 16:44:37 -0400

    using albumentations on image and mask simultaneouly to get correct tensor dims

--- known_good/train.py
+++ known_good/train.py
@@ -95,3 +95,95 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# import torch
+# import albumentations as A
+# from albumentations.pytorch import ToTensorV2
+# from tqdm import tqdm
+# import torch.nn as nn
+# import torch.optim as optim
+# # from model import UNET
+# from model_known_good import UNET
+
+# from utils import (
+#     get_loaders,
+#     check_accuracy,
+#     save_predictions_as_imgs,
+# )
+
+# # Hyperparameters etc.
+# lr = 1e-6
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# batch_size = 1
+# epochs = 1
+# img_height = 160
+# img_width = 240
+
+
+# def main():
+#     train_transform = A.Compose(
+#         [
+#             A.Resize(height=img_height, width=img_width),
+#             A.Normalize(
+#                 mean=[0.0, 0.0, 0.0],
+#                 std=[1.0, 1.0, 1.0],
+#                 max_pixel_value=255.0,
+#             ),
+#             ToTensorV2(),
+#         ],
+#     )
+
+#     test_transform = A.Compose(
+#         [
+#             A.Resize(height=img_height, width=img_width),
+#             A.Normalize(
+#                 mean=[0.0, 0.0, 0.0],
+#                 std=[1.0, 1.0, 1.0],
+#                 max_pixel_value=255.0,
+#             ),
+#             ToTensorV2(),
+#         ],
+#     )
+
+#     model = UNET(in_channels=3, out_channels=1).to(device)
+#     loss_fun = nn.BCEWithLogitsLoss()
+#     optimizer = optim.Adam(model.parameters(), lr=lr)
+
+#     train_loader, test_loader = get_loaders(
+#         batch_size,
+#         train_transform,
+#         test_transform,
+#     )
+
+#     check_accuracy(test_loader, model, device=device)
+
+#     for epoch in range(epochs):
+#         for (pixel_data, target_masks) in train_loader:
+#             pixel_data = pixel_data.to(device=device)
+#             print(pixel_data.size())
+#             target_masks = target_masks.float().unsqueeze(1).to(device=device)
+#             print(target_masks.size())
+
+#             # forward
+#             predictions = model(pixel_data)
+#             print(predictions.size())
+
+#             loss = loss_fun(predictions, target_masks)
+
+#             # backward
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+
+#         # check accuracy
+#         check_accuracy(test_loader, model, device=device)
+
+#         # print some examples to a folder
+#         save_predictions_as_imgs(
+#             test_loader, model, folder=my_path + "//saved_images//", device=device
+#         )
+
+
+# if __name__ == "__main__":
+#     main()
--- known_good/utils.py
+++ known_good/utils.py
@@ -30,18 +30,22 @@ def get_loaders(
 
     # train images
     train_x = []
-    count = 0
-    for path in os.listdir(my_path + "\\data\\train_images\\"):
-        inputPath = os.path.join(my_path + "\\data\\train_images\\", path)
+    train_y = []
 
-        img = Image.open(inputPath)
+    for (path_img, path_mask) in zip(os.listdir(my_path + "\\data\\train_images\\"), os.listdir(my_path + "\\data\\train_masks\\")):
+        full_path_img = os.path.join(
+            my_path + "\\data\\train_images\\", path_img)
+        full_path_mask = os.path.join(
+            my_path + "\\data\\train_masks\\", path_mask)
 
         from skimage.io import imread
 
         # im = imageio.imread(inputPath)
         # im = np.array(im)
 
-        image = np.array(Image.open(inputPath).convert("RGB"))
+        image = np.array(Image.open(full_path_img).convert("RGB"))
+        mask = np.array(Image.open(full_path_mask).convert("L"),
+                        dtype=np.float32)
 
         train_transform = A.Compose(
             [
@@ -55,10 +59,11 @@ def get_loaders(
             ],
         )
 
-        augmentations = train_transform(image=image)
+        augmentations = train_transform(image=image, mask=mask)
         # print(np.shape(augmentations["image"]))
 
         train_x.append(augmentations["image"])
+        train_y.append(augmentations["mask"])
         # count += 1
         # print("count: ", count)
 
@@ -71,47 +76,46 @@ def get_loaders(
     # train_x = augmentations["train_x"]
     # print(np.shape(train_data))
 
-    train_y = []
-    count = 0
-    for path in os.listdir(my_path + "\\data\\train_masks\\"):
-        inputPath = os.path.join(my_path + "\\data\\train_masks\\", path)
+    # count = 0
+    # for path_img in os.listdir(my_path + "\\data\\train_masks\\"):
+    #     full_path_img = os.path.join(my_path + "\\data\\train_masks\\", path)
 
-        img = Image.open(inputPath)
+    #     img = Image.open(inputPath)
 
-        from skimage.io import imread
+    #     from skimage.io import imread
 
-        # im = imageio.imread(inputPath)
-        # # print(np.shape(im))
-        # train_y.append(im)
-        # mask = np.array(Image.open(inputPath).convert("L"), dtype=np.float32)
+    #     # im = imageio.imread(inputPath)
+    #     # # print(np.shape(im))
+    #     # train_y.append(im)
+    #     # mask = np.array(Image.open(inputPath).convert("L"), dtype=np.float32)
 
-        image = np.array(Image.open(inputPath).convert("RGB"))
-        # image = np.array(Image.open(inputPath).convert("L"), dtype=np.float32)
+    #     # image = np.array(Image.open(inputPath).convert("RGB"))
+    #     mask = np.array(Image.open(inputPath).convert("L"), dtype=np.float32)
 
-        # print(image)
+    #     # print(image)
 
-        train_transform = A.Compose(
-            [
-                A.Resize(height=160, width=240),
-                A.Normalize(
-                    mean=[0.0, 0.0, 0.0],
-                    std=[1.0, 1.0, 1.0],
-                    max_pixel_value=255.0,
-                ),
-                ToTensorV2(),
-            ],
-        )
+    #     train_transform = A.Compose(
+    #         [
+    #             A.Resize(height=160, width=240),
+    #             A.Normalize(
+    #                 mean=[0.0, 0.0, 0.0],
+    #                 std=[1.0, 1.0, 1.0],
+    #                 max_pixel_value=255.0,
+    #             ),
+    #             ToTensorV2(),
+    #         ],
+    #     )
 
-        augmentations = train_transform(image=image)
+    #     augmentations = train_transform(mask=mask)
 
-        train_y.append(augmentations["image"])
-        # count += 1
-    # print("count: ", count)
+    #     train_y.append(augmentations["mask"])
+    #     # count += 1
+    # # print("count: ", count)
 
-    # train_y = np.array(train_y)
-    # print(train_y)
-    # print(type(train_x))
-    # print(type(train_y))
+    # # train_y = np.array(train_y)
+    # # print(train_y)
+    # # print(type(train_x))
+    # # print(type(train_y))
 
     train_x = torch.stack(train_x)
     train_y = torch.stack(train_y)
@@ -134,79 +138,115 @@ def get_loaders(
     #     # transform=test_transform,
     # )
 
-    test_x = []
-    count = 0
-    for path in os.listdir(my_path + "\\data\\test_images\\"):
-        inputPath = os.path.join(my_path + "\\data\\test_images\\", path)
+    # test_x = []
+    # count = 0
+    # for img_path in os.listdir(my_path + "\\data\\test_images\\"):
+    #     inputPath = os.path.join(my_path + "\\data\\test_images\\", path)
 
-        img = Image.open(inputPath)
+    #     img = Image.open(inputPath)
 
-        from skimage.io import imread
+    #     from skimage.io import imread
 
-        # im = imageio.imread(inputPath)
-        # # print(np.shape(im))
-        # test_x.append(im)
-        test_transform = A.Compose(
-            [
-                A.Resize(height=160, width=240),
-                A.Normalize(
-                    mean=[0.0, 0.0, 0.0],
-                    std=[1.0, 1.0, 1.0],
-                    max_pixel_value=255.0,
-                ),
-                ToTensorV2(),
-            ],
-        )
+    #     # im = imageio.imread(inputPath)
+    #     # # print(np.shape(im))
+    #     # test_x.append(im)
+    #     test_transform = A.Compose(
+    #         [
+    #             A.Resize(height=160, width=240),
+    #             A.Normalize(
+    #                 mean=[0.0, 0.0, 0.0],
+    #                 std=[1.0, 1.0, 1.0],
+    #                 max_pixel_value=255.0,
+    #             ),
+    #             ToTensorV2(),
+    #         ],
+    #     )
 
-        image = np.array(Image.open(inputPath).convert("RGB"))
+    #     image = np.array(Image.open(inputPath).convert("RGB"))
 
-        augmentations = test_transform(image=image)
+    #     augmentations = test_transform(image=image)
 
-        test_x.append(augmentations["image"])
-        # count += 1
-    # print("count: ", count)
+    #     test_x.append(augmentations["image"])
+    #     # count += 1
+    # # print("count: ", count)
 
-    # test_x = np.array(test_x)
-    # print(np.shape(test_data))
+    # # test_x = np.array(test_x)
+    # # print(np.shape(test_data))
 
-    test_y = []
-    count = 0
-    for path in os.listdir(my_path + "\\data\\test_masks\\"):
-        inputPath = os.path.join(my_path + "\\data\\test_masks\\", path)
+    # test_y = []
+    # count = 0
+    # for path in os.listdir(my_path + "\\data\\test_masks\\"):
+    #     inputPath = os.path.join(my_path + "\\data\\test_masks\\", path)
 
-        img = Image.open(inputPath)
+    #     img = Image.open(inputPath)
 
-        from skimage.io import imread
+    #     from skimage.io import imread
 
-        # im = imageio.imread(inputPath)
-        # # print(np.shape(im))
-        # test_y.append(im)
+    #     # im = imageio.imread(inputPath)
+    #     # # print(np.shape(im))
+    #     # test_y.append(im)
 
-        test_transform = A.Compose(
-            [
-                A.Resize(height=160, width=240),
-                A.Normalize(
-                    mean=[0.0, 0.0, 0.0],
-                    std=[1.0, 1.0, 1.0],
-                    max_pixel_value=255.0,
-                ),
-                ToTensorV2(),
-            ],
-        )
+    #     test_transform = A.Compose(
+    #         [
+    #             A.Resize(height=160, width=240),
+    #             A.Normalize(
+    #                 mean=[0.0, 0.0, 0.0],
+    #                 std=[1.0, 1.0, 1.0],
+    #                 max_pixel_value=255.0,
+    #             ),
+    #             ToTensorV2(),
+    #         ],
+    #     )
 
-        image = np.array(Image.open(inputPath).convert("RGB"))
-        # image = np.array(Image.open(inputPath).convert("L"), dtype=np.float32)
+    #     # image = np.array(Image.open(inputPath).convert("RGB"))
+    #     image = np.array(Image.open(inputPath).convert("L"), dtype=np.float32)
 
-        augmentations = test_transform(image=image)
+    #     augmentations = test_transform(image=image)
 
-        test_y.append(augmentations["image"])
-        # count += 1
-    # print("count: ", count)
+    #     test_y.append(augmentations["image"])
+    #     # count += 1
+    # # print("count: ", count)
 
     # test_y = np.array(test_y)
 
     # test_data = TensorDataset(torch.from_numpy(
     #     test_x), torch.from_numpy(test_y))
+
+    test_x = []
+    test_y = []
+
+    for (path_img, path_mask) in zip(os.listdir(my_path + "\\data\\test_images\\"), os.listdir(my_path + "\\data\\test_masks\\")):
+        full_path_img = os.path.join(
+            my_path + "\\data\\test_images\\", path_img)
+        full_path_mask = os.path.join(
+            my_path + "\\data\\test_masks\\", path_mask)
+
+        from skimage.io import imread
+
+        # im = imageio.imread(inputPath)
+        # im = np.array(im)
+
+        image = np.array(Image.open(full_path_img).convert("RGB"))
+        mask = np.array(Image.open(full_path_mask).convert("L"),
+                        dtype=np.float32)
+
+        test_transform = A.Compose(
+            [
+                A.Resize(height=160, width=240),
+                A.Normalize(
+                    mean=[0.0, 0.0, 0.0],
+                    std=[1.0, 1.0, 1.0],
+                    max_pixel_value=255.0,
+                ),
+                ToTensorV2(),
+            ],
+        )
+
+        augmentations = test_transform(image=image, mask=mask)
+        # print(np.shape(augmentations["image"]))
+
+        test_x.append(augmentations["image"])
+        test_y.append(augmentations["mask"])
 
     test_x = torch.stack(test_x)
     test_y = torch.stack(test_y)
@@ -271,3 +311,120 @@ def save_predictions_as_imgs(
         count = count + 1
 
     model.train()
+
+# import torch
+# import torchvision
+# from dataset import getBluenoImages
+# from torch.utils.data import DataLoader
+# from PIL import Image
+# import imageio
+
+# import numpy as np
+
+# import os
+
+# my_path = os.path.dirname(__file__)
+
+
+# def get_loaders(
+#     batch_size,
+#     train_transform,
+#     test_transform,
+#     num_workers=4,
+#     pin_memory=True,
+# ):
+#     train_ds = getBluenoImages(
+#         image_dir=my_path + "\\data\\train_images\\",
+#         mask_dir=my_path + "\\data\\train_masks\\",
+#         transform=train_transform,
+#     )
+
+#     # train images
+#     train_data = []
+#     count = 0
+#     for path in os.listdir(my_path + "\\data\\train_images\\"):
+#         inputPath = os.path.join(my_path + "\\data\\train_images\\", path)
+
+#         img = Image.open(inputPath)
+
+#         from skimage.io import imread
+
+#         im = imageio.imread(inputPath)
+#         # print(np.shape(im))
+#         train_data.append(im)
+#         count += 1
+#     print("count: ", count)
+
+#     train_data = np.array(train_data)
+#     print(np.shape(train_data))
+
+#     train_loader = DataLoader(
+#         train_ds,
+#         batch_size=batch_size,
+#         num_workers=num_workers,
+#         pin_memory=pin_memory,
+#         shuffle=True,
+#     )
+
+#     test_ds = getBluenoImages(
+#         image_dir=my_path + "\\data\\test_images\\",
+#         mask_dir=my_path + "\\data\\test_masks\\",
+#         transform=test_transform,
+#     )
+
+#     test_loader = DataLoader(
+#         test_ds,
+#         batch_size=batch_size,
+#         num_workers=num_workers,
+#         pin_memory=pin_memory,
+#         shuffle=False,
+#     )
+
+#     return train_loader, test_loader
+
+
+# def check_accuracy(loader, model, device="cuda"):
+#     num_correct = 0
+#     num_pixels = 0
+#     dice_score = 0
+#     model.eval()
+
+#     with torch.no_grad():
+#         for x, y in loader:
+#             x = x.to(device)
+#             y = y.to(device).unsqueeze(1)
+#             preds = torch.sigmoid(model(x))
+#             preds = (preds > 0.5).float()
+#             num_correct += (preds == y).sum()
+#             num_pixels += torch.numel(preds)
+#             dice_score += (2 * (preds * y).sum()) / (
+#                 (preds + y).sum() + 1e-8
+#             )
+
+#     print(
+#         f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+#     )
+#     print(f"Dice score: {dice_score/len(loader)}")
+#     model.train()
+
+
+# def save_predictions_as_imgs(
+#     loader, model, folder="saved_images/", device="cuda"
+# ):
+#     model.eval()
+#     count = 0
+#     for idx, (x, y) in enumerate(loader):
+#         if count > 10:
+#             break
+#         else:
+#             x = x.to(device=device)
+#             with torch.no_grad():
+#                 preds = torch.sigmoid(model(x))
+#                 preds = (preds > 0.5).float()
+#             torchvision.utils.save_image(
+#                 preds, f"{folder}/pred_{idx}.png"
+#             )
+#             torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
+#         count = count + 1
+
+#     model.train()
