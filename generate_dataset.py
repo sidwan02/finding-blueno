b659ebcc5424ea088dc57344af73adb73fd4b5dc
Siddharth Diwan <sidwan02@gmail.com>
2021-04-26 16:02:14 -0400

    added img generation ability to randomly scale + translate blueno over rndm bg

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,51 +1,85 @@
 from PIL import Image
 import os
 from PIL import ImageFilter
+import random
 
 my_path = os.path.dirname(__file__)
 
 
 class ProcessDataset():
-    def __init__(self, src_path, target_image_path, target_mask_path, predict):
+    def __init__(self, src_path, target_image_path, target_mask_path, restricted):
         self.src_path = src_path
         self.target_image_path = target_image_path
         self.target_mask_path = target_mask_path
-        self.predict = predict
-        self.process_images_in_folder()
+        self.restricted = restricted
 
-    def process_images_in_folder(self):
         for path in os.listdir(self.src_path):
-            if predict == False:
-                inputPath = os.path.join(self.src_path, path)
+            inputPath = os.path.join(self.src_path, path)
 
-                # inputPath contains the full directory name
-                img = Image.open(inputPath)
+            # inputPath contains the full directory name
+            img = Image.open(inputPath)
 
-                img = img.resize((160, 240))
+            img = img.resize((240, 160))
 
-                processed_image_path = os.path.join(
-                    self.target_image_path, 'processed_' + path)
-                mask_path = os.path.join(self.target_mask_path, 'mask_' + path)
-                processed_img = self.superimpose_bleuno(img)
-                processed_img.save(processed_image_path)
+            processed_image_path = os.path.join(
+                self.target_image_path, 'processed_' + path)
+            mask_path = os.path.join(self.target_mask_path, 'mask_' + path)
 
-            mask = self.generate_mask(255)
+            (scale_factor, horizontal_pos,
+             vertical_pos) = self.get_img_superposition_state()
+            processed_img = self.superimpose_bleuno(
+                background=img, scale_factor=scale_factor, horizontal_pos=horizontal_pos,
+                vertical_pos=vertical_pos)
+
+            processed_img.save(processed_image_path)
+
+            mask = self.generate_mask(thresh=255, scale_factor=scale_factor, horizontal_pos=horizontal_pos,
+                                      vertical_pos=vertical_pos)
 
             mask.save(mask_path)
+            # print("here")
+            break
 
-    def superimpose_bleuno(self, background):
+    def get_img_superposition_state(self):
+        if self.restricted:  # centered
+            scale_factor = 100
+        else:
+            scale_factor = random.uniform(10, 160)
+        # print(scale_factor)
+
+        if self.restricted:  # monosize
+            horizontal_pos = 70
+            vertical_pos = 40
+            # align_position = (70, 40)
+        else:
+            horizontal_pos = random.uniform(0, 240 - scale_factor)
+            vertical_pos = random.uniform(0, 160 - scale_factor)
+            # align_position = (int(horizontal_pos), int(vertical_pos))
+
+        return (scale_factor, horizontal_pos, vertical_pos)
+
+    def superimpose_bleuno(self, background, scale_factor, horizontal_pos, vertical_pos):
         foreground = Image.open(
             my_path + "/blueno_original/blueno_cropped.png")
 
-        foreground.thumbnail((100, 100))
+        scale_position = (scale_factor, scale_factor)
 
-        background.paste(foreground, (100, 80), foreground)
+        foreground.thumbnail(scale_position)
+
+        align_position = (int(horizontal_pos), int(vertical_pos))
+        # print(align_position)
+
+        background.paste(foreground, align_position, foreground)
+        # print(background)
         return background
         # background.show()
 
-    def generate_mask(self, thresh):
-        background = Image.new('RGB', (160, 240), (255, 255, 255))
-        img = self.superimpose_bleuno(background)
+    def generate_mask(self, thresh, scale_factor, horizontal_pos,
+                      vertical_pos):
+        background = Image.new('RGB', (240, 160), (255, 255, 255))
+        img = self.superimpose_bleuno(
+            background=background, scale_factor=scale_factor, horizontal_pos=horizontal_pos,
+            vertical_pos=vertical_pos)
 
         def fn(x): return 255 if x < thresh else 0
 
@@ -54,5 +88,7 @@ class ProcessDataset():
         return mask
 
 
-ProcessDataset(my_path + "/background_images/images", target_image_path=my_path +
-               "/processed_images/images", target_mask_path=my_path + "/processed_images/masks", False)
+ProcessDataset(src_path=my_path + "/background_images/images",
+               target_image_path=my_path + "/processed_images/images",
+               target_mask_path=my_path + "/processed_images/masks",
+               restricted=False)
